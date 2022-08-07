0853d1de8fabaa4854e175df70f5b8dff1975063
nkere30 <ninokereselidze30@gmail.com>
2022-08-08 00:28:21 +0400

    Completed

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,8 +2,6 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public static void swap(int first, int second) {
-        System.out.println(first);
-        System.out.println(second);
         first = first ^ second;
         second = first ^ second;
         first = first ^ second;
