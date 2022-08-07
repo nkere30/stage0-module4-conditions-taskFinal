2516f9953e3a9f718a175fa9abc05827057f19e5
nkere30 <ninokereselidze30@gmail.com>
2022-08-08 00:25:39 +0400

    Completed

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,17 +2,24 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public static void printCompletelyDivided(int dividend, int divider) {
-        int division = dividend/divider;
-        boolean dividedCompletely = divider * division == dividend;
-        int dividedCompletelyToInt = dividedCompletely? 1:0;
-        String result = switch(dividedCompletelyToInt) {
-            case 1 -> {
-                yield "can be divided completely";
+        String result = "";
+        if(divider != 0) {
+            int division = dividend/divider;
+            boolean dividedCompletely = divider * division == dividend;
+            int dividedCompletelyToInt = dividedCompletely? 1:0;
+            switch(dividedCompletelyToInt) {
+                case 1:
+                    result += "can be divided completely";
+                    break;
+                case 0:
+                    result += "cannot be divided completely";
+                    break;
+                default:
+                    break;
             }
-            default -> {
-                yield "cannot be divided completely";
-            }
-        };
+        } else {
+            result += "division by zero";
+        }
         System.out.println(result);
     }
 
@@ -20,6 +27,6 @@ public class IntegerDivider {
         printCompletelyDivided(6, 3);
         printCompletelyDivided(7, 4);
         printCompletelyDivided(4, 4);
-        printCompletelyDivided(4, 8);
+        printCompletelyDivided(5, 0);
     }
 }
